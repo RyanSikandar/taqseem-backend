5c908871235ab2a722a675f32ae73cdaf2a03cd4
RyanSikandar <ryansikandar@gmail.com>
2025-01-25 14:34:31 +0500

    chore:refactor

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const authRoutes = require('./routes/AuthRoutes')
 const app = express();
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: false }));
-
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
@@ -14,8 +10,8 @@ const PORT = process.env.PORT || 3000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT,() => {
-     console.log(`Running Server on port: ${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Running Server on port: ${PORT}`);
       console.log("MongoDB Connected!");
     });
   })
