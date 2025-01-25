9c4ba48d3b2eec2455cc4b6b510642d68eab36a8
RyanSikandar <ryansikandar@gmail.com>
2025-01-25 14:43:29 +0500

    chore: minor refactor

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const authRoutes = require('./routes/AuthRoutes')
 const app = express();
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
